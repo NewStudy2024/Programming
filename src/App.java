8179736b729c14a2a101963ed5dee9ecdb284222
Ngoc901 <huanikii@gmail.com>
2024-12-22 19:50:14 +0100

    Added diamond method which prints a diamond pattern

--- src/App.java
+++ src/App.java
@@ -3,12 +3,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        diamond();
         Scanner scanner = new Scanner(System.in);
-        greetingAggressively();
-        square(scanner);
-        triangle(scanner);
-        christmasTree(scanner);
+       
         scanner.close();
 
     }
@@ -46,13 +43,48 @@ public class App {
         }
     }
 
-    public static void triangle(Scanner scanner) {
+    public static void triangle() {
         for(int i = 0; i< 3; i++){
             for(int j = 0; j<= i; j++){
              System.out.print("*");
             }
             System.err.println();
          }
+    }
+
+    public static void diamond() {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (n-i) ; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j <= 2*i ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < (n-i) ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= n; i++) {
+           
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 0; j <= 2*(n-i) ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
     }
 
     public static void christmasTree(Scanner scanner) {
@@ -76,4 +108,13 @@ public class App {
             System.out.println("*");
          }
     }
+
+        
+            
+           
+        
+    
+
+    
 }
+
