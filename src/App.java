a53caea262f655410cdefa65e8a3b34f8690ff31
Ngoc901 <huanikii@gmail.com>
2024-12-24 22:27:37 +0100

    Update README.md with new information

--- src/App.java
+++ src/App.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        matrixTranspose();
         Scanner scanner = new Scanner(System.in);
-       
+        diamond();
         scanner.close();
 
     }
 
     public static void greetingAggressively(){
-         //printing Hello World
+         // Printing Hello World
          System.out.println("Hello, World!");
 
-         //Basic for loop
+         // Printing Hello World 5 times
          for (int i = 1; i <= 5; i++) {
              System.out.println("Hello, World!");
          }
     }
-
+    // Method to print a rectangle based on user input
     public static void square(Scanner scanner){
         System.out.print("Enter the number of rows: ");
         int rows = scanner.nextInt();
@@ -28,13 +27,13 @@ public class App {
         int columns = scanner.nextInt();
 
         String[][] matrix = new String[rows][columns];
-
+        // Filling matrix
         for(int i = 0; i< rows; i++){
             for(int j = 0; j< columns; j++){
                 matrix[i][j] = "*";
             }
         }
-
+        // Printing
         for(int i = 0; i< matrix.length; i++){
             for(int j = 0; j< matrix[i].length; j++){
                 System.out.print(matrix[i][j] + " ");
@@ -42,7 +41,7 @@ public class App {
             System.out.println();
         }
     }
-
+    // Method to print a simple triangle of stars
     public static void triangle() {
         for(int i = 0; i< 3; i++){
             for(int j = 0; j<= i; j++){
@@ -51,16 +50,18 @@ public class App {
             System.err.println();
          }
     }
-
+      // Method to print a diamond pattern of stars
     public static void diamond() {
-        int n = 5;
+        int n = 5; //size of diamond
+
+        // Top half of the diamond
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < (n-i) ; j++) {
                 System.out.print("*");
             }
 
             for (int j = 0; j <= 2*i ; j++) {
-                System.out.print(" ");
+                System.out.print(" "); // Print spaces
             }
 
             for (int j = 0; j < (n-i) ; j++) {
@@ -68,7 +69,7 @@ public class App {
             }
             System.out.println();
         }
-
+        //Lower half of the diamond
         for (int i = 1; i <= n; i++) {
            
             for (int j = 1; j <= i; j++) {
@@ -86,21 +87,22 @@ public class App {
             System.out.println();
         }
     }
-
+     // Method to print a Christmas tree based on user input
     public static void christmasTree(Scanner scanner) {
-        System.out.print("Enter the height of the tree: ");
+        System.out.print("Enter the height of the tree: "); // Get height of the tree
         int height = scanner.nextInt();        
 
+         // Print the tree
         for(int i = 0; i< height-1; i++){
             for(int j = 0; j< height - i -1; j++){
-                System.err.print(" ");
+                System.err.print(" "); // Print spaces
             }
             for(int j = 0; j< 2*i+1; j++){
-                System.out.print("*");
+                System.out.print("*"); // Print stars
             }
             System.out.println();
          }
-         
+         // Print the tree trunk
          for(int i = 0; i<2; i++){
             for(int j = 0; j< height -1; j++){
                 System.err.print(" ");
@@ -108,7 +110,7 @@ public class App {
             System.out.println("*");
          }
     }
-
+     // Method to print a matrix
    public static void printMatrix(int [][] matrix ){
         
         for (int row = 0; row<matrix.length; row++){
@@ -119,7 +121,7 @@ public class App {
         }
     
    }
-
+   // Method to add two matrices
    public static void matrixAddition() {
         int [][] matrix1 = {
             {1, 2,},
@@ -141,7 +143,7 @@ public class App {
 
         printMatrix(matrix3);
    }
-        
+    // Method to multiply two matrices
     public static void matrixMultiplication() {
         //2x3
         int [][] matrix1 = {
@@ -170,7 +172,7 @@ public class App {
 
         printMatrix(matrix3);
     }
-
+     // Transpose the matrix
     public static void matrixTranspose() {
         int [][] matrix1 = {
             {1, 1, 1, 1},
