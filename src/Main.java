2b59af707e6e0369480d1c8905607154b69b343f
Ngoc901 <huanikii@gmail.com>
2025-01-16 18:19:39 +0100

    put everything in methods: getPlayerChoice, getResult, getDecisionInText, getCompCHoice, getDecision and in separate classes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.lang.runtime.SwitchBootstraps;
 import java.util.Scanner;
 import java.util.Random;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
@@ -19,7 +16,9 @@ public class Main {
         //Ticket calculator
         ticketCalculator();*/
 
-        rockPaperScissors();
+        System.out.println("Launching Rock Paper Scissors...");
+        RockPaperScissors rockPaperScissors = new RockPaperScissors();
+        rockPaperScissors.startGame();
 
     }
 
@@ -54,87 +53,13 @@ public class Main {
 
     }
 
-    public static void rockPaperScissors(){
-        Scanner scanner = new Scanner(System.in);
-
-        String[] choices = {"rock", "paper", "scissors"};
-        int compWin;
-        int playerWin;
-        boolean wantExit = false;
-
-
-        String playersChoice = scanner.nextLine();
-        playersChoice = playersChoice.toLowerCase();
-
-
-
-           System.out.println("-----------------------");
-           System.out.println("Welcome to Rock Paper Scissors!)");
-           System.out.println("Please enter your choice:  / Rock / Paper / Scissors / Exit): ");
-           playersChoice = scanner.nextLine();
-
-           int compChoice = getCompMove();
-           int playerChoice = getPlayerMove(playersChoice);
-
-           getDecision(compChoice, playerChoice);
 
 
 
 
-    }
 
-    public static int getCompMove(){
-        Random random = new Random();
-        int move = random.nextInt(3)+1;
-        return move;
-    }
 
-    public static int getPlayerMove(String playerChoice){
-        int move;
-        switch(playerChoice) {
-            case "rock":
-                return move = 1; //rock
-            case "paper":
-                return move = 2; //paper
-            default:
-                return move = 3; //scissors
-        }
-    }
 
-    public static void getDecision (int compMove, int playerMove){
-
-        switch(compMove) {
-            case 1: //rock
-                if(playerMove == 1){
-                    System.out.println("It's a tie!");
-                }else if(playerMove == 2){
-                    System.out.println("You won!");
-                }else if(playerMove == 3){
-                    System.out.println("Computer won!");
-                }
-            case 2: //paper
-                if(playerMove == 1){
-                    System.out.println("Computer won!");
-                }else if(playerMove == 2){
-                    System.out.println("It's a tie!");
-                }else if(playerMove == 3){
-                    System.out.println("You won!");
-                }
-            default: //scissors
-                if(playerMove == 1){
-                    System.out.println("You won!");
-                }else if(playerMove == 2){
-                    System.out.println("Computer won!");
-                }else if(playerMove == 3){
-                    System.out.println("It's a tie!");
-                }
-        }
-
-    }
-
-    public static void getResult (String choice, int player){
-
-    }
 
 
 
