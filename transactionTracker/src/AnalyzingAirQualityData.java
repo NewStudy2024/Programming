3f3dd33a6028f6eb8590465b3dcd29314dda5e50
Ngoc901 <huanikii@gmail.com>
2025-01-17 19:47:04 +0100

    transactionTracker

---
+++ transactionTracker/src/AnalyzingAirQualityData.java
@@ -0,0 +1,11 @@
+import java.net.http.*;
+import java.net.URI;
+import java.io.IOException;
+
+public class AnalyzingAirQualityData {
+    private static final String API_KEY = System.getenv("API_KEY");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder();
+    }
+
+}
